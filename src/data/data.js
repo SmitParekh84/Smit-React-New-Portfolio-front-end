83fe2109cf4a76d7daafbb7b786aeb45fcbfbf16
Smit Parekh <smitparekh02@gmail.com>
2024-09-28 11:01:33 +0530

    imge fix

--- src/data/data.js
+++ src/data/data.js
@@ -1,4 +1,6 @@
 // data.js
+
+
 export const homeData = {
   title: "Hi, I'm Smit Parekh",
   subtitle: "Marketing Manager/Node.js Developer",
@@ -263,35 +265,39 @@ export const portfolioData = [
 ];
 
 
+import PreetImage from '../assets/img/Preet.png';
+import DhairyaImage from '../assets/img/dhairya.jpg';
+import DhruImage from '../assets/img/dhru.jpg';
+import TirthImage from '../assets/img/tirth.jpg';
 
 export const testimonialsData = [
   {
     name: "Preet Patel",
     client: "Java Heaven Pvt.",
-    image: "../assets/img/Preet.png",
+    image: PreetImage,
     description: "Smit's expertise in web development transformed our online presence. He delivered high-quality work on time and was always available for support. Highly recommended!",
-    rating: 4, // Add rating
+    rating: 4,
   },
   {
     name: "Dhairya Mehata",
     client: "Techno Hub",
-    image: "../assets/img/dhairya.jpg",
+    image: DhairyaImage,
     description: "Working with Smit was a game changer for our marketing strategy. His creative insights and attention to detail helped us reach our goals effectively. We're extremely pleased with the results!",
-    rating: 4, // Add rating
+    rating: 4,
   },
   {
     name: "Dhru Patel",
     client: "Tech Innovations",
-    image: "../assets/img/dhru.jpg",
+    image: DhruImage,
     description: "Smit consistently goes above and beyond. His ability to understand our needs and deliver tailored solutions made the entire process smooth and enjoyable. I can’t imagine working with anyone else!",
-    rating: 5, // Add rating
+    rating: 5,
   },
   {
     name: "Tirth Bhavsar",
     client: "GreenTech Ltd.",
-    image: "../assets/img/tirth.jpg",
+    image: TirthImage,
     description: "From the initial consultation to the final product, Smit was professional and communicative. His dedication to excellence truly stands out, and he has been a valuable partner for our projects.",
-    rating: 4, // Add rating
+    rating: 4,
   },
 ];
 
